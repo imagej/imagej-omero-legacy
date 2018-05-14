3d68592c0ac0d1faceb161378d05009d3a45462d
Alison Walter <awalter2@wisc.edu>
2018-05-14 01:21:02 -0500

    Add converters which preserve additional settings

---
+++ src/main/java/net/imagej/omero/legacy/OMERORealMaskToIJROI.java
@@ -0,0 +1,272 @@
+
+package net.imagej.omero.legacy;
+
+import ij.gui.Line;
+import ij.gui.OvalRoi;
+import ij.gui.PointRoi;
+import ij.gui.PolygonRoi;
+import ij.gui.Roi;
+
+import net.imagej.legacy.convert.roi.box.BoxWrapper;
+import net.imagej.legacy.convert.roi.ellipsoid.EllipsoidWrapper;
+import net.imagej.legacy.convert.roi.line.LineWrapper;
+import net.imagej.legacy.convert.roi.point.PointMaskWrapper;
+import net.imagej.legacy.convert.roi.polygon2d.Polygon2DWrapper;
+import net.imagej.legacy.convert.roi.polyline.PolylineWrapper;
+import net.imagej.omero.roi.OMERORealMask;
+import net.imagej.omero.roi.ellipse.OMEROEllipse;
+import net.imagej.omero.roi.line.OMEROLine;
+import net.imagej.omero.roi.point.OMEROPoint;
+import net.imagej.omero.roi.polygon.OMEROPolygon;
+import net.imagej.omero.roi.polyline.OMEROPolyline;
+import net.imagej.omero.roi.rectangle.OMERORectangle;
+import net.imglib2.roi.MaskPredicate;
+
+import ome.model.units.BigResult;
+import omero.gateway.model.ShapeData;
+import omero.model.enums.UnitsLength;
+
+import org.scijava.Priority;
+import org.scijava.convert.AbstractConverter;
+import org.scijava.convert.Converter;
+import org.scijava.plugin.Plugin;
+
+/**
+ * Converters for {@link OMERORealMask} to {@link Roi}. These converters attempt
+ * to preserve some of the additional information represented in
+ * {@link ShapeData} but not in {@link MaskPredicate}.
+ *
+ * @author Alison Walter
+ */
+public final class OMERORealMaskToIJROI {
+
+	private OMERORealMaskToIJROI() {
+		// NB: prevent instantiation of base class
+	}
+
+	/**
+	 * Abstract base class for converting {@link OMERORealMask} to {@link Roi}.
+	 *
+	 * @param <O> the type of {@link OMERORealMask}
+	 * @param <R> the type of {@link Roi}
+	 */
+	public static abstract class AbstractOMERORealMaskToIJRoi<O extends OMERORealMask<?>, R extends Roi>
+		extends AbstractConverter<O, R>
+	{
+
+		@Override
+		@SuppressWarnings("unchecked")
+		public <T> T convert(final Object src, final Class<T> dest) {
+			if (src == null || dest == null) throw new NullPointerException();
+			if (!getInputType().isInstance(src)) {
+				throw new IllegalArgumentException("Expected: " + getInputType()
+					.getSimpleName() + " Received: " + src.getClass().getSimpleName());
+			}
+			if (!dest.isAssignableFrom(getOutputType())) {
+				throw new IllegalArgumentException("Expected: " + getOutputType()
+					.getSimpleName() + " Received: " + dest.getSimpleName());
+			}
+
+			final O omeroRoi = (O) src;
+			final ShapeData shape = omeroRoi.getShape();
+			final R ijRoi = wrap(omeroRoi);
+
+			// set position
+			ijRoi.setPosition(shape.getC() + 1, shape.getZ() + 1, shape.getT() + 1);
+
+			// store position in case un-intentionally overwritten
+			ijRoi.setProperty(LegacyOMEROROIService.OMERO_ROI_Z, Long.toString(shape
+				.getZ()));
+			ijRoi.setProperty(LegacyOMEROROIService.OMERO_ROI_T, Long.toString(shape
+				.getT()));
+			ijRoi.setProperty(LegacyOMEROROIService.OMERO_ROI_C, Long.toString(shape
+				.getC()));
+
+			// set style
+			try {
+				if (shape.getShapeSettings().getStrokeWidth(UnitsLength.PIXEL) != null)
+					ijRoi.setStrokeWidth(shape.getShapeSettings().getStrokeWidth(
+						UnitsLength.PIXEL).getValue());
+			}
+			catch (final BigResult exc) {
+				// Do nothing. Stroke width won't be preserved
+			}
+			if (shape.getShapeSettings().getStroke() != null) ijRoi.setStrokeColor(
+				shape.getShapeSettings().getStroke());
+			if (shape.getShapeSettings().getFill() != null) ijRoi.setFillColor(shape
+				.getShapeSettings().getFill());
+
+			// set name
+			if (getText(omeroRoi) != null && !getText(omeroRoi).isEmpty()) ijRoi
+				.setName(getText(omeroRoi));
+
+			return (T) ijRoi;
+		}
+
+		public abstract R wrap(final O omeroRoi);
+
+		public abstract String getText(final O omeroRoi);
+	}
+
+	/** Converts {@link OMEROEllipse} to {@link OvalRoi}. */
+	@Plugin(type = Converter.class, priority = Priority.VERY_HIGH)
+	public static class OMEROEllipseToOvalRoi extends
+		AbstractOMERORealMaskToIJRoi<OMEROEllipse, OvalRoi>
+	{
+
+		@Override
+		public Class<OvalRoi> getOutputType() {
+			return OvalRoi.class;
+		}
+
+		@Override
+		public Class<OMEROEllipse> getInputType() {
+			return OMEROEllipse.class;
+		}
+
+		@Override
+		public OvalRoi wrap(final OMEROEllipse omeroRoi) {
+			return new EllipsoidWrapper(omeroRoi);
+		}
+
+		@Override
+		public String getText(final OMEROEllipse omeroRoi) {
+			return omeroRoi.getShape().getText();
+		}
+	}
+
+	/** Converts {@link OMEROLine} to {@link ij.gui.Line}. */
+	@Plugin(type = Converter.class, priority = Priority.VERY_HIGH)
+	public static class OMEROLineToIJLine extends
+		AbstractOMERORealMaskToIJRoi<OMEROLine, ij.gui.Line>
+	{
+
+		@Override
+		public Class<ij.gui.Line> getOutputType() {
+			return ij.gui.Line.class;
+		}
+
+		@Override
+		public Class<OMEROLine> getInputType() {
+			return OMEROLine.class;
+		}
+
+		@Override
+		public Line wrap(final OMEROLine omeroRoi) {
+			return new LineWrapper(omeroRoi);
+		}
+
+		@Override
+		public String getText(final OMEROLine omeroRoi) {
+			return omeroRoi.getShape().getText();
+		}
+	}
+
+	/** Converts {@link OMEROPoint} to {@link PointRoi}. */
+	@Plugin(type = Converter.class, priority = Priority.VERY_HIGH)
+	public static class OMEROPointToPointRoi extends
+		AbstractOMERORealMaskToIJRoi<OMEROPoint, PointRoi>
+	{
+
+		@Override
+		public Class<PointRoi> getOutputType() {
+			return PointRoi.class;
+		}
+
+		@Override
+		public Class<OMEROPoint> getInputType() {
+			return OMEROPoint.class;
+		}
+
+		@Override
+		public PointRoi wrap(final OMEROPoint omeroRoi) {
+			return new PointMaskWrapper(omeroRoi);
+		}
+
+		@Override
+		public String getText(final OMEROPoint omeroRoi) {
+			return omeroRoi.getShape().getText();
+		}
+	}
+
+	/** Converts {@link OMEROPolygon} to {@link PolygonRoi}. */
+	@Plugin(type = Converter.class, priority = Priority.VERY_HIGH)
+	public static class OMEROPolygonToPolygonRoi extends
+		AbstractOMERORealMaskToIJRoi<OMEROPolygon, PolygonRoi>
+	{
+
+		@Override
+		public Class<PolygonRoi> getOutputType() {
+			return PolygonRoi.class;
+		}
+
+		@Override
+		public Class<OMEROPolygon> getInputType() {
+			return OMEROPolygon.class;
+		}
+
+		@Override
+		public PolygonRoi wrap(final OMEROPolygon omeroRoi) {
+			return new Polygon2DWrapper(omeroRoi);
+		}
+
+		@Override
+		public String getText(final OMEROPolygon omeroRoi) {
+			return omeroRoi.getShape().getText();
+		}
+	}
+
+	/** Converts {@link OMEROPolyline} to {@link PolygonRoi}. */
+	@Plugin(type = Converter.class, priority = Priority.VERY_HIGH)
+	public static class OMEROPolylineToPolygonRoi extends
+		AbstractOMERORealMaskToIJRoi<OMEROPolyline, PolygonRoi>
+	{
+
+		@Override
+		public Class<PolygonRoi> getOutputType() {
+			return PolygonRoi.class;
+		}
+
+		@Override
+		public Class<OMEROPolyline> getInputType() {
+			return OMEROPolyline.class;
+		}
+
+		@Override
+		public PolygonRoi wrap(final OMEROPolyline omeroRoi) {
+			return new PolylineWrapper(omeroRoi);
+		}
+
+		@Override
+		public String getText(final OMEROPolyline omeroRoi) {
+			return omeroRoi.getShape().getText();
+		}
+	}
+
+	/** Converts {@link OMERORectangle} to {@link Roi}. */
+	@Plugin(type = Converter.class, priority = Priority.VERY_HIGH)
+	public static class OMERORectangleToRoi extends
+		AbstractOMERORealMaskToIJRoi<OMERORectangle, Roi>
+	{
+
+		@Override
+		public Class<OMERORectangle> getInputType() {
+			return OMERORectangle.class;
+		}
+
+		@Override
+		public Class<Roi> getOutputType() {
+			return Roi.class;
+		}
+
+		@Override
+		public Roi wrap(final OMERORectangle omeroRoi) {
+			return new BoxWrapper(omeroRoi);
+		}
+
+		@Override
+		public String getText(final OMERORectangle omeroRoi) {
+			return omeroRoi.getShape().getText();
+		}
+	}
+}
---
+++ src/main/java/net/imagej/omero/legacy/WrappedOMERORealMaskToMaskPredicate.java
@@ -0,0 +1,317 @@
+
+package net.imagej.omero.legacy;
+
+import java.lang.reflect.Type;
+
+import ij.IJ;
+import ij.Prefs;
+import ij.gui.Roi;
+
+import net.imagej.legacy.convert.roi.MaskPredicateWrapper;
+import net.imagej.legacy.convert.roi.box.BoxWrapper;
+import net.imagej.legacy.convert.roi.ellipsoid.EllipsoidWrapper;
+import net.imagej.legacy.convert.roi.line.LineWrapper;
+import net.imagej.legacy.convert.roi.point.PointMaskWrapper;
+import net.imagej.legacy.convert.roi.polygon2d.Polygon2DWrapper;
+import net.imagej.legacy.convert.roi.polyline.PolylineWrapper;
+import net.imagej.omero.roi.OMERORealMask;
+import net.imagej.omero.roi.ellipse.OMEROEllipse;
+import net.imagej.omero.roi.line.OMEROLine;
+import net.imagej.omero.roi.point.OMEROPoint;
+import net.imagej.omero.roi.polygon.OMEROPolygon;
+import net.imagej.omero.roi.polyline.OMEROPolyline;
+import net.imagej.omero.roi.rectangle.OMERORectangle;
+import net.imglib2.roi.MaskPredicate;
+
+import ome.formats.model.UnitsFactory;
+import omero.RInt;
+import omero.gateway.model.EllipseData;
+import omero.gateway.model.LineData;
+import omero.gateway.model.PointData;
+import omero.gateway.model.PolygonData;
+import omero.gateway.model.PolylineData;
+import omero.gateway.model.RectangleData;
+import omero.gateway.model.ShapeData;
+import omero.model.LengthI;
+import omero.model.Shape;
+
+import org.scijava.Priority;
+import org.scijava.convert.AbstractConverter;
+import org.scijava.convert.Converter;
+import org.scijava.log.LogService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+/**
+ * Converts a {@link MaskPredicateWrapper} which wraps an {@link OMERORealMask}
+ * to a {@link MaskPredicate}. These conversions preserve some of the meta-data
+ * presented in {@link ShapeData} and {@link Roi}, but not in
+ * {@link MaskPredicate}.
+ *
+ * @author Alison Walter
+ */
+public final class WrappedOMERORealMaskToMaskPredicate {
+
+	private WrappedOMERORealMaskToMaskPredicate() {
+		// NB: prevent instantiation of base class
+	}
+
+	/**
+	 * Abstract base class for converting wrapped {@link OMERORealMask}s to
+	 * {@link OMERORealMask}.
+	 *
+	 * @param <W> type of {@link MaskPredicateWrapper}, the type must also be a
+	 *          {@link Roi}.
+	 * @param <O> type of {@link OMERORealMask} output.
+	 */
+	public static abstract class AbstractWrappedOMERORealMaskToMaskPredicate<W extends MaskPredicateWrapper<?>, O extends OMERORealMask<?>>
+		extends AbstractConverter<W, O>
+	{
+
+		@Parameter
+		private LogService log;
+
+		@Override
+		public boolean canConvert(final Object src, final Type dest) {
+			return super.canConvert(src, dest) && getOutputType().isInstance(
+				((MaskPredicateWrapper<?>) src).getSource()) && src instanceof Roi;
+		}
+
+		@Override
+		public boolean canConvert(final Object src, final Class<?> dest) {
+			return super.canConvert(src, dest) && getOutputType().isInstance(
+				((MaskPredicateWrapper<?>) src).getSource()) && src instanceof Roi;
+		}
+
+		@Override
+		@SuppressWarnings("unchecked")
+		public <T> T convert(final Object src, final Class<T> dest) {
+			if (src == null || dest == null) throw new NullPointerException();
+			if (!getInputType().isInstance(src)) {
+				throw new IllegalArgumentException("Expected: " + getInputType()
+					.getSimpleName() + " Received: " + src.getClass().getSimpleName());
+			}
+			if (!dest.isAssignableFrom(getOutputType())) {
+				throw new IllegalArgumentException("Expected: " + getOutputType()
+					.getSimpleName() + " Received: " + dest.getSimpleName());
+			}
+			if (!(src instanceof Roi)) throw new IllegalArgumentException(
+				"MaskPredicateWrapper must also be instance of ij.gui.Roi!");
+
+			final Roi ijRoi = (Roi) src;
+			final ShapeData omeroRoi =
+				((OMERORealMask<?>) ((MaskPredicateWrapper<?>) src).getSource())
+					.getShape();
+
+			// Set position
+			// NB: If showAllSliceOnly and isMacro are both false, ImageJ overwrites
+			// the position
+			boolean ignoreIJPosition = false;
+			if (!Prefs.showAllSliceOnly && !IJ.isMacro())
+				ignoreIJPosition = true;
+
+			final Shape s = (Shape) omeroRoi.asIObject();
+			s.setTheZ(computePosition(ignoreIJPosition, ijRoi.getProperty(
+				LegacyOMEROROIService.OMERO_ROI_Z), ijRoi.getZPosition()));
+			s.setTheT(computePosition(ignoreIJPosition, ijRoi.getProperty(
+				LegacyOMEROROIService.OMERO_ROI_T), ijRoi.getTPosition()));
+			s.setTheC(computePosition(ignoreIJPosition, ijRoi.getProperty(
+				LegacyOMEROROIService.OMERO_ROI_C), ijRoi.getCPosition()));
+
+			if (ijRoi.getStrokeWidth() > 0) omeroRoi.getShapeSettings()
+				.setStrokeWidth(new LengthI(ijRoi.getStrokeWidth(),
+					UnitsFactory.Shape_StrokeWidth));
+			if (ijRoi.getStrokeColor() != null) omeroRoi.getShapeSettings().setStroke(
+				ijRoi.getStrokeColor());
+			if (ijRoi.getFillColor() != null) omeroRoi.getShapeSettings().setFill(
+				ijRoi.getFillColor());
+			setText((W) src);
+
+			return (T) ((W) src).getUpdatedSource();
+		}
+
+		public abstract void setText(W wrapper);
+
+		private RInt computePosition(final boolean ignoreIJPos,
+			final String omeroPos, final int ijPos)
+		{
+			if (ignoreIJPos && omeroPos != null && !omeroPos.isEmpty()) {
+				final int pos = Integer.parseInt(omeroPos);
+				// NB: Do NOT set this to n -1, null always!
+				if (pos < 0) return null;
+				return omero.rtypes.rint(pos);
+			}
+			if (ijPos == 0) return null;
+			return omero.rtypes.rint(ijPos - 1);
+		}
+	}
+
+	/**
+	 * Converts {@link EllipsoidWrapper} which wraps {@link OMEROEllipse} to
+	 * {@link OMEROEllipse}.
+	 */
+	@Plugin(type = Converter.class, priority = Priority.VERY_HIGH)
+	public static class WrappedOMEROEllipseToOMEROEllipse extends
+		AbstractWrappedOMERORealMaskToMaskPredicate<EllipsoidWrapper, OMEROEllipse>
+	{
+
+		@Override
+		public Class<OMEROEllipse> getOutputType() {
+			return OMEROEllipse.class;
+		}
+
+		@Override
+		public Class<EllipsoidWrapper> getInputType() {
+			return EllipsoidWrapper.class;
+		}
+
+		@Override
+		public void setText(final EllipsoidWrapper wrapper) {
+			if (wrapper.getName() != null && !wrapper.getName().isEmpty()) {
+				final EllipseData e = ((OMEROEllipse) wrapper.getSource()).getShape();
+				e.setText(wrapper.getName());
+			}
+		}
+	}
+
+	/**
+	 * Converts {@link LineWrapper} which wraps {@link OMEROLine} to
+	 * {@link OMEROLine}.
+	 */
+	@Plugin(type = Converter.class, priority = Priority.VERY_HIGH)
+	public static class WrappedOMEROLineToOMEROLine extends
+		AbstractWrappedOMERORealMaskToMaskPredicate<LineWrapper, OMEROLine>
+	{
+
+		@Override
+		public Class<OMEROLine> getOutputType() {
+			return OMEROLine.class;
+		}
+
+		@Override
+		public Class<LineWrapper> getInputType() {
+			return LineWrapper.class;
+		}
+
+		@Override
+		public void setText(final LineWrapper wrapper) {
+			if (wrapper.getName() != null && !wrapper.getName().isEmpty()) {
+				final LineData d = ((OMEROLine) wrapper.getSource()).getShape();
+				d.setText(wrapper.getName());
+			}
+		}
+	}
+
+	/**
+	 * Converts {@link PointMaskWrapper} which wraps {@link OMEROPoint} to
+	 * {@link OMEROPoint}.
+	 */
+	@Plugin(type = Converter.class, priority = Priority.VERY_HIGH)
+	public static class WrappedOMEROPointToOMEROPoint extends
+		AbstractWrappedOMERORealMaskToMaskPredicate<PointMaskWrapper, OMEROPoint>
+	{
+
+		@Override
+		public Class<OMEROPoint> getOutputType() {
+			return OMEROPoint.class;
+		}
+
+		@Override
+		public Class<PointMaskWrapper> getInputType() {
+			return PointMaskWrapper.class;
+		}
+
+		@Override
+		public void setText(final PointMaskWrapper wrapper) {
+			if (wrapper.getName() != null && !wrapper.getName().isEmpty()) {
+				final PointData d = ((OMEROPoint) wrapper.getSource()).getShape();
+				d.setText(wrapper.getName());
+			}
+		}
+	}
+
+	/**
+	 * Converts {@link PolylineWrapper} which wraps {@link OMEROPolyline} to
+	 * {@link OMEROPolyline}.
+	 */
+	@Plugin(type = Converter.class, priority = Priority.VERY_HIGH)
+	public static class WrappedOMEROPolylineToOMEROPolyline extends
+		AbstractWrappedOMERORealMaskToMaskPredicate<PolylineWrapper, OMEROPolyline>
+	{
+
+		@Override
+		public Class<OMEROPolyline> getOutputType() {
+			return OMEROPolyline.class;
+		}
+
+		@Override
+		public Class<PolylineWrapper> getInputType() {
+			return PolylineWrapper.class;
+		}
+
+		@Override
+		public void setText(final PolylineWrapper wrapper) {
+			if (wrapper.getName() != null && !wrapper.getName().isEmpty()) {
+				final PolylineData d = ((OMEROPolyline) wrapper.getSource()).getShape();
+				d.setText(wrapper.getName());
+			}
+		}
+	}
+
+	/**
+	 * Converts {@link Polygon2DWrapper} which wraps {@link OMEROPolygon} to
+	 * {@link OMEROPolygon}.
+	 */
+	@Plugin(type = Converter.class, priority = Priority.VERY_HIGH)
+	public static class WrappedOMEROPolygonToOMEROPolygon extends
+		AbstractWrappedOMERORealMaskToMaskPredicate<Polygon2DWrapper, OMEROPolygon>
+	{
+
+		@Override
+		public Class<OMEROPolygon> getOutputType() {
+			return OMEROPolygon.class;
+		}
+
+		@Override
+		public Class<Polygon2DWrapper> getInputType() {
+			return Polygon2DWrapper.class;
+		}
+
+		@Override
+		public void setText(final Polygon2DWrapper wrapper) {
+			if (wrapper.getName() != null && !wrapper.getName().isEmpty()) {
+				final PolygonData d = ((OMEROPolygon) wrapper.getSource()).getShape();
+				d.setText(wrapper.getName());
+			}
+		}
+	}
+
+	/**
+	 * Converts {@link BoxWrapper} which wraps {@link OMERORectangle} to
+	 * {@link OMERORectangle}.
+	 */
+	@Plugin(type = Converter.class, priority = Priority.VERY_HIGH)
+	public static class WrappedOMERORectangleToOMERORectangle extends
+		AbstractWrappedOMERORealMaskToMaskPredicate<BoxWrapper, OMERORectangle>
+	{
+
+		@Override
+		public Class<OMERORectangle> getOutputType() {
+			return OMERORectangle.class;
+		}
+
+		@Override
+		public Class<BoxWrapper> getInputType() {
+			return BoxWrapper.class;
+		}
+
+		@Override
+		public void setText(final BoxWrapper wrapper) {
+			if (wrapper.getName() != null && !wrapper.getName().isEmpty()) {
+				final RectangleData d = ((OMERORectangle) wrapper.getSource())
+					.getShape();
+				d.setText(wrapper.getName());
+			}
+		}
+	}
+}
