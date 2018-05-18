19cdb0681539e6c5276d64ebfd33056c020dbfd7
Alison Walter <awalter2@wisc.edu>
2018-05-18 12:00:13 -0500

    Add logic to preserve font characteristics for text

--- src/main/java/net/imagej/omero/legacy/text/OMEROTextWrapper.java
+++ src/main/java/net/imagej/omero/legacy/text/OMEROTextWrapper.java
@@ -5,6 +5,10 @@ import ij.gui.TextRoi;
 
 import net.imagej.legacy.convert.roi.MaskPredicateWrapper;
 
+import ome.model.enums.UnitsLength;
+import omero.gateway.model.ShapeSettingsData;
+import omero.model.LengthI;
+
 /**
  * Wraps an {@link OMEROText} as an ImageJ 1.x ROI.
  *
@@ -17,7 +21,8 @@ public class OMEROTextWrapper extends TextRoi implements
 	private final OMEROText text;
 
 	public OMEROTextWrapper(final OMEROText text) {
-		super(text.getX(), text.getY(), text.getText());
+		super(text.getX(), text.getY(), text.getText(), text.getShape()
+			.getShapeSettings().getFont());
 		this.text = text;
 	}
 
@@ -31,5 +36,22 @@ public class OMEROTextWrapper extends TextRoi implements
 		text.setText(getText());
 		text.setX(getXBase());
 		text.setY(getYBase());
+		text.getShape().getShapeSettings().setFontFamily(getCurrentFont()
+			.getFamily());
+		text.getShape().getShapeSettings().setFontSize(new LengthI(getCurrentFont()
+			.getSize(), UnitsLength.POINT));
+		text.getShape().getShapeSettings().setFontStyle(computeStyle());
+	}
+
+	// -- Helper methods --
+
+	private String computeStyle() {
+		if (getCurrentFont().isBold()) {
+			if (getCurrentFont().isItalic())
+				return ShapeSettingsData.FONT_BOLD_ITALIC;
+			return ShapeSettingsData.FONT_BOLD;
+		}
+		else if (getCurrentFont().isItalic()) return ShapeSettingsData.FONT_ITALIC;
+		return ShapeSettingsData.FONT_REGULAR;
 	}
 }
